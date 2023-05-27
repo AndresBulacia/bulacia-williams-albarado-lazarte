d1ed53ed1bd7e7b7c60525f883db87a730faa088
AndresBulacia <andresbulacia040@gmail.com>
2023-05-27 15:48:03 -0300

    Layout,Lateral,Contenido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Header from "./components/Header"
 import Grupos from "./components/Grupos";
 import React from "react";
+import Card from "./components/Card";
+
 function App() {
   let tituloPagina = "Trabajo Practico N°1"
   return (
     <div className="App">
       <div className="container">
         <div className="item c1"><Header titulo = {tituloPagina}/></div>
-        <div className="item c2">Menú</div>
+        <div className="item c2">Menu</div>
         <div className="item c3"><p><Grupos/></p></div>
-        <div className="item c4">Barra lateral</div>
+        <div className="item c4"><Card/></div>
         <div className="item c5">Footer</div>
       </div>
     </div>
